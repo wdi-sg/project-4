5a966903b1add5c8b554a9aca03c3f1c253ac1bb
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-08-29 20:18:14 +0800

    routing part 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
-import Home from './Home'
-import German from './languages/German'
 import './App.css'
+
+import Home from './Home'
+import Contact from './Contact'
+import German from './languages/German'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+  } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <Home></Home>
-        <German></German>
+
+        <Router>
+          <div>
+            <nav>
+              <Link to="/german">Go to German Page</Link>{' '}
+              <Link to="/">Go to Home Page</Link>{' '}
+              <Link to="/contact">Contact Us</Link>{' '}
+            </nav>
+              <Route exact path="/" component={Home} />
+              <Route path="/german" component={German} />
+              <Route path="/contact" component={Contact} />
+          </div>
+        </Router>
       </div>
     )
   }
