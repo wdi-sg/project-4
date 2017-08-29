67357f474d1cc0024d15136cd0ac4d04e6ef393e
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-08-29 19:27:18 +0800

    routing part 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Home from './Home'
+import German from './languages/German'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Home></Home>
+        <German></German>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/languages/German.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class German extends Component {
+  render () {
+    return (
+      <div>
+        <h1>German</h1>
+      </div>
+    )
+  }
+}
+
+export default German
