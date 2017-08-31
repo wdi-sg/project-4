052d0749f58e4d082d188b547c4b8e8ebcf301be
Shaun Loh <loh.shaun89@gmail.com>
2017-08-31 16:11:49 +0800

    working livestock page

--- src/components/protected/Graph.js
+++ src/components/protected/Graph.js
@@ -1,52 +1,11 @@
-import React, {Component} from 'react'
-import Livestock from './Livestock'
-import ReactDom from 'react-dom'
+import React, { Component } from 'react'
 
-class Graph extends Component {
-  constructor (props) {
-    super()
+const Graph = (props) => (
+  <div>
+    <li>
+      {props.time}:{props.rsi}
+    </li>
+  </div>
+)
 
-    this.state = {
-      rsiArr: []
-    }
-  }
-  render () {
-    let allRSI = this.state.rsiArr.map((rsi, index) => {
-      return <Livestock key={index} rsi={rsi} />
-    })
-    return (
-      <div>
-        <h2>latest RSI to past 100 RSI</h2>
-        <ol>
-          {allRSI}
-        </ol>
-      </div>
-    )
-  }
-
-  componentDidMount () {
-    const url = 'https://www.alphavantage.co/query?function=RSI&symbol=AAPL&interval=daily&time_period=10&series_type=close&apikey=D2E5ZAQU25U0NKAE'
-
-    fetch(url)
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        var rsiObj = (data['Technical Analysis: RSI'])
-        var allRsiArr = []
-        for (var prop in rsiObj) {
-          allRsiArr.push(rsiObj[prop])
-        }
-        allRsiArr.map((rsi, index) => {
-          if (index < 5) {
-            this.setState({
-              rsiArr: this.state.rsiArr.concat(rsi.RSI)
-            })
-          }
-        })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-}
+export default Graph
--- src/components/protected/Livestock.js
+++ src/components/protected/Livestock.js
@@ -1,9 +1,66 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import Graph from './Graph'
 
-const Livestock = (props) => (
-  <li>
-    {props.rsi}
-  </li>
-)
+class Livestock extends Component {
+  constructor (props) {
+    super()
+
+    this.state = {
+      rsiArr: [],
+      timeArr: []
+    }
+  }
+  render () {
+    let allRSI = this.state.rsiArr.map((rsi, index) => {
+      return <Graph key={index} rsi={rsi} />
+    })
+    let allTime = this.state.timeArr.map((time, index) => {
+      return <Graph key={index} time={time} />
+    })
+    return (
+      <div>
+        <h2>RSI</h2>
+        <ol>
+          {allTime}
+        </ol>
+      </div>
+    )
+  }
+
+  componentDidMount () {
+    const url = 'https://www.alphavantage.co/query?function=RSI&symbol=AAPL&interval=daily&time_period=10&series_type=close&apikey=D2E5ZAQU25U0NKAE'
+
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        var rsiObj = (data['Technical Analysis: RSI'])
+        var allRsiArr = []
+        var allTimeArr = []
+        for (var prop in rsiObj) {
+          allRsiArr.push(rsiObj[prop])
+          allTimeArr.push(prop)
+        }
+        allRsiArr.map((rsi, index) => {
+          if (index < 10) {
+            this.setState({
+              rsiArr: this.state.rsiArr.concat(rsi.RSI)
+            })
+          }
+        })
+        allTimeArr.map((time, index) => {
+          if (index < 10) {
+            this.setState({
+              timeArr: this.state.timeArr.concat(time)
+            })
+          }
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
 
 export default Livestock
