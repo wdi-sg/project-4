592b06564bd14039b4fcc22a1024f1ff70ad8573
Nicholas Lee <niconal902@gmail.com>
2017-08-31 15:17:09 +0800

    allowed user to view all other users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users,
-          path: '',
-          path_names: {
-            sign_in: 'login',
-            sign_out: 'logout',
-            sign_up: 'register'
-          }
 
+  # get 'users/index'
+  match '/users',   to: 'users#index',   via: 'get'
+
+  match '/users/:id',     to: 'users#show',       via: 'get'
+
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only =>[:show]
+
+  # devise_for :users,
+  #         path: '',
+  #         path_names: {
+  #           sign_in: 'login',
+  #           sign_out: 'logout',
+  #           sign_up: 'register'
+  #         }
 end
