ae87d03937f9d6950f7fc65be72e526e802ace13
Nicholas Lee <niconal902@gmail.com>
2017-08-31 12:11:30 +0800

    added name field to register

--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20170831034721) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
