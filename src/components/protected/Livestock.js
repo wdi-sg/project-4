e815b151e4c295b8129c9d98531f7aa5765bb0b3
lee brian <leesinshingbrian@gmail.com>
2017-09-04 01:09:09 +0800

    api for stock price can be called

--- src/components/protected/Livestock.js
+++ src/components/protected/Livestock.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Graph from './Graph'
+import Price from './Price'
 
 class Livestock extends Component {
   constructor (props) {
@@ -7,10 +8,18 @@ class Livestock extends Component {
 
     this.state = {
       rsiArr: [],
-      timeArr: []
+      timeArr: [],
+      priceArr: [],
+      timePeriod: '',
+      timeSeries: '',
+      arrSize: ''
     }
+    this.handleChange = this.handleChange.bind(this)
   }
   render () {
+    let allPrice = this.state.priceArr.map((price, index) => {
+      return <Price key={index} price={price} />
+    })
     let allData = this.state.rsiArr.map((rsi, index) => {
       // let graphObj = {
       //   x: this.state.timeArr[index],
@@ -31,8 +40,39 @@ class Livestock extends Component {
     // let allTime = this .state.timeArr.map((time, index) => {
     //   return <Graph key={index} time={time} />
     // })
+    const option1 = {
+      timePeriod: 'INTRADAY&interval=5min&outputsize=full',
+      timeSeries: 'Time Series (5min)',
+      arrSize: 288
+    }
+    const option2 = {
+      timePeriod: 'INTRADAY&interval=15min&outputsize=full',
+      timeSeries: 'Time Series (15min)',
+      arrSize: 480
+    }
     return (
       <div>
+        <h2>Price of Stock: </h2>
+        <form onChange={this.handleChange}>
+          <label>
+            Please select time period:
+            <select>
+              <option value={option1}>24 hrs</option>
+              <option value={option2}>5 days</option>
+              <option value='INTRADAY&interval=60min&outputsize=full'>1 month</option>
+              <option value='DAILY&outputsize=full'>3 months</option>
+              <option value='DAILY&outputsize=full'>6 months</option>
+              <option value='DAILY&outputsize=full'>1 year</option>
+              <option value='WEEKLY'>5 years</option>
+              <option value='WEEKLY'>10 years</option>
+              <option value='MONTHLY'>All Data</option>
+            </select>
+          </label>
+        </form>
+        <h2>Prices</h2>
+        <ol>
+          {allPrice}
+        </ol>
         <h2>RSI</h2>
         <ol>
           {allData}
@@ -40,6 +80,34 @@ class Livestock extends Component {
         {/* <ul>{allRSI}</ul> */}
       </div>
     )
+  }
+
+  handleChange (e) {
+    let random = e.target.value
+    console.log(random)
+    let base = this
+
+    var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_' + e.target.value.timePeriod + '&symbol=AAPL&apikey=D2E5ZAQU25U0NKAE'
+
+    fetch(url)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          var obj = (data['Time Series (5min)'])
+          var randomArr = []
+          for (var prop in obj) {
+            randomArr.push(obj[prop])
+          }
+          randomArr.map((price, index) => {
+            if (index < 5) {
+              base.setState({
+                priceArr:
+                this.state.priceArr.concat(price['4. close'])
+              })
+            }
+          })
+        })
   }
 
   componentDidMount () {
