547f9f455893c102c20664648f831dfa5e455a7f
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-09-03 13:51:46 +0800

    added lang info file

---
+++ src/languages/langinfo.js
@@ -0,0 +1,16 @@
+export const lang = {
+  german: {
+    country: 'Germany, Austria, Switzerland, Luxembourg, Liechtenstein',
+    speakers: '95 million people',
+    easy: 'Uses the latin script and shares many similar words with English, What you read is what you speak',
+    hard: 'The Grammer! 1. 3 genders (die, das, der), 2. 4 cases (Nominativ, Akkusativ, Dativ, Genitiv), 3. Word order: German sentences have a strange habit of throwing verbs to the end of long sentences, or separating one verb and putting the pieces at opposite ends of the sentence. 4. Plurals: There is very little pattern to turning singular nouns into plurals, so you almost have to learn every noun twice.',
+    why: '1. Germany is an economic powerhouse, 2. Low tuition fees in German universities, 3. Germans are everywhere'
+  },
+  japanese: {
+    country: 'Japan',
+    speakers: '125 million speakers',
+    easy: 'The pronunciation is standard so it is easy to pick at the language at the beginning',
+    hard: 'It has 3 scripts to learn (Hiragana, Katakana, Kanji) also for each Kanji character there are multiple ways to read it, 3 levels of formality (casual, formal, business formal) of which the business formal is like a different vocabulary altogether',
+    why: '1. There are many Japanese companies across the world, 2. Japan has a very unique and interesting culture'
+  }
+}
--- src/schools/schoolinfo.js
+++ src/schools/schoolinfo.js
@@ -5,6 +5,7 @@ export const school = {
     location: '136 Neil Road, 088865',
     trainStation: 'Outram Park',
     skillsfuture: 'No',
+    languages: 'German',
     materials: 'https://www.cornelsen.de/studio_21/',
     link: 'https://www.goethe.de/ins/sg/en/spr/kur/gia.html',
     schedule: {
@@ -26,6 +27,7 @@ export const school = {
     location: '51 Cuppage Road, #10-12, Singapore 229469',
     trainStation: 'Somerset',
     skillsfuture: 'Yes',
+    languages: 'Chinese, Dutch, English, French, German, Hindi, Indonesian, Italian, Japanese, Korean, Malay, Russian, Spanish, Thai, Vietnamese',
     materials: 'http://www.inlingua.edu.sg/languages/german/part-time-tuition/contents-materials',
     link: 'http://www.inlingua.edu.sg/',
     schedule: {
