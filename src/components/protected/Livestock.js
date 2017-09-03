d29d7a787e9da8c7c04106c1f4cef95e082b6f74
lee brian <leesinshingbrian@gmail.com>
2017-09-03 21:56:44 +0800

    before merging shaun renamed files

--- src/components/protected/Livestock.js
+++ src/components/protected/Livestock.js
@@ -1,9 +1,54 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import Graph from './Graph'
+import ReactDom from 'react-dom'
 
-const Livestock = (props) => (
-  <li>
-    {props.rsi}
-  </li>
-)
+class Livestock extends Component {
+  constructor (props) {
+    super()
+
+    this.state = {
+      rsiArr: []
+    }
+  }
+  render () {
+    let allRSI = this.state.rsiArr.map((rsi, index) => {
+      return <Graph key={index} rsi={rsi} />
+    })
+    return (
+      <div>
+        <h2>latest RSI to past 100 RSI</h2>
+        <ol>
+          {allRSI}
+        </ol>
+      </div>
+    )
+  }
+
+  componentDidMount () {
+    const url = 'https://www.alphavantage.co/query?function=RSI&symbol=AAPL&interval=daily&time_period=10&series_type=close&apikey=D2E5ZAQU25U0NKAE'
+
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        var rsiObj = (data['Technical Analysis: RSI'])
+        var allRsiArr = []
+        for (var prop in rsiObj) {
+          allRsiArr.push(rsiObj[prop])
+        }
+        allRsiArr.map((rsi, index) => {
+          if (index < 5) {
+            this.setState({
+              rsiArr: this.state.rsiArr.concat(rsi.RSI)
+            })
+          }
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
 
 export default Livestock
