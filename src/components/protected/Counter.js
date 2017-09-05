e074b9afc96590bd329445fedc54c40445e23bea
lee brian <leesinshingbrian@gmail.com>
2017-09-05 19:51:11 +0800

    cleand up some stuff

--- src/components/protected/Counter.js
+++ src/components/protected/Counter.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import Enemy from './Enemy'
-import SimpleLinearRegression from 'ml-regression-simple-linear';
+import SimpleLinearRegression from 'ml-regression-simple-linear'
 import Generate from './Generate'
 
 class Counter extends Component {
   constructor (props) {
     super(props)
 
-    this.state ={
+    this.state = {
       priceArr: [],
       rsiArr: props.rsi
     }
@@ -19,41 +19,36 @@ class Counter extends Component {
     })
   }
 
-
-  render() {
-
-
-
+  render () {
     let allPrices = this.state.priceArr.map((number, index) => {
-        return <Enemy key={index} name={number} />
+      return <Enemy key={index} name={number} />
     })
 
     return (
       <div>
-      {/* <ul>Price: {allPrices}</ul> */}
-      <Generate x={this.state.priceArr} y={this.state.rsiArr}/>
+        {/* <ul>Price: {allPrices}</ul> */}
+        <Generate x={this.state.priceArr} y={this.state.rsiArr} />
 
       </div>
     )
   }
-componentDidMount () {
-  const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=15min&outputsize=full&apikey=D2E5ZAQU25U0NKAE'
+  componentDidMount () {
+    const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=15min&outputsize=full&apikey=D2E5ZAQU25U0NKAE'
 
-  fetch(url)
+    fetch(url)
     .then((response) => { // promise is resolved, and response is received
       // console.log('response', response)
       return response.json() // convert response.body into json format
     })
 
-    .then ((data) => {
-      var obj =(data["Time Series (15min)"])
-      var randomArr=[]
+    .then((data) => {
+      var obj = (data['Time Series (15min)'])
+      var randomArr = []
       for (var prop in obj) {
         randomArr.push(obj[prop])
       }
-      console.log(this.state.priceArr);
-      const newArr = randomArr.slice(0,200).map((indicator, index) => {
-        return indicator[("4. close")]
+      const newArr = randomArr.slice(0, 200).map((indicator, index) => {
+        return indicator[('4. close')]
       })
       this.setState({
         priceArr: newArr
@@ -64,10 +59,8 @@ componentDidMount () {
     .catch((err) => {
       console.log('err', err) // just in case if api call fails
     })
-}
+  }
 
 }
-
-
 
 export default Counter
