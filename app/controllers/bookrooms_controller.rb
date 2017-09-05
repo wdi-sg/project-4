2cdf1a257a3914888c30654cdfbb96f7a07b098b
Nicholas Lee <niconal902@gmail.com>
2017-09-05 12:10:27 +0800

    created meetingrooms

--- app/controllers/bookrooms_controller.rb
+++ app/controllers/bookrooms_controller.rb
@@ -3,41 +3,11 @@ class BookroomsController < ApplicationController
   end
 
   def create
-    # render json: params
-    #
-    times = params[:bookroom][:slot]
 
-    times.each_with_index do |i, index|
-      if (i.length > 0)
-        @new_room = Bookroom.create(params.require(:bookroom).permit(:meetingroom_id, :date_start, :slot, :user_id, :price))
-        @new_room.user_id = 1
-
-        room = Meetingroom.find_by(room_title: params[:bookroom][:meetingroom])
-        @new_room.meetingroom_id = room.id
-
-        @new_room.price = 10
-
-        # @slot = params[:bookroom][:slot][index]
-        slot = Slot.find_by(time_start: params[:bookroom][:slot][index])
-        @new_room.slot = slot.id
-        @new_room.save!
-      end
-    end
-    render json: @new_room
   end
 
   def new
-    @new_room = Bookroom.new
-    @room_title = Meetingroom.distinct.pluck(:room_title).sort
 
-    @taken_slots = Bookroom.distinct.pluck(:date_start)
-    @taken_slots = Bookroom.where(date_start: @taken_slots)
-
-    @slot = Slot.all
-    @slot_avail = []
-    @slot.each do |i|
-      @slot_avail << i.time_start
-    end
   end
 
   def edit
