5a88e16aeecd5eceee9123df9e6c015a00ab4a28
Shaun Loh <loh.shaun89@gmail.com>
2017-09-05 15:03:24 +0800

    able to show dates on RSI x-axis

--- src/components/protected/Livestock.js
+++ src/components/protected/Livestock.js
@@ -122,9 +122,9 @@ class Livestock extends Component {
         </ol>
 
         <h2>RSI</h2>
-        <LineChart width={1000} height={500} data={rsiData}
+        <LineChart width={1000} height={300} data={rsiData}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <XAxis dataKey='name' padding={{ right: 20 }} />
+          <XAxis dataKey='date' padding={{ right: 20 }} />
           <YAxis type='number' domain={['dataMin - 2', 'dataMax + 2']} padding={{ top: 20, bottom: 20 }} />
           <CartesianGrid strokeDasharray='3 3' />
           <Tooltip />
@@ -183,6 +183,17 @@ class Livestock extends Component {
         var rsiObj = (data['Technical Analysis: RSI'])
         var allRsiArr = []
         var allTimeArr = []
+        var timeRsiArr = []
+        for (var prop in rsiObj) {
+          timeRsiArr.push({date: prop, RSI: +rsiObj[prop].RSI })
+        }
+        timeRsiArr.map((timeRsiData, index) => {
+          if (index < 10) {
+            this.setState({
+              rsiDataArr: this.state.rsiDataArr.concat(timeRsiData)
+            })
+          }
+        })
         for (var prop in rsiObj) {
           allRsiArr.push(rsiObj[prop])
           allTimeArr.push(prop)
@@ -191,8 +202,7 @@ class Livestock extends Component {
         allRsiArr.map((rsi, index) => {
           if (index < 10) {
             this.setState({
-              rsiArr: this.state.rsiArr.concat(+rsi.RSI),
-              rsiDataArr: this.state.rsiDataArr.concat({ name: index, RSI: +rsi.RSI })
+              rsiArr: this.state.rsiArr.concat(+rsi.RSI)
             })
           }
         })
