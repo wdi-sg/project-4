546d86f272ee80476da54900e2e923adab81fc9e
Daniel Lim <ddanielnp@hotmail.com>
2017-09-06 17:12:58 +0800

    grouped items in user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,28 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(params[:id])
     @bookings = Bookroom.where(user_id: @user)
+
+    @user = User.find_by_id(params[:id])
+    @booking_groups_by_date_start =
+    Bookroom.select('user_id, date_start, count(*) as total_bookings')
+            .group('user_id', 'date_start')
+            .having('user_id = ?', current_user.id)
+
+    @allBookingsPerGroup = []
+    @booking_groups_by_date_start.each do |group|
+      @allBookingsPerGroup << Bookroom.where(user_id: current_user.id, date_start: group.date_start)
+    end
+
+
+
+
+
     # @bookings_date = Bookroom.where(user_id: @user, date_start: "09/09/2017")
     # @dategroup = Bookroom.includes(:date_start)
     @events = Bookevent.where(user_id: @user)
-@all_events = Event.where('event_start>?', DateTime.now.change(:offset => "+0000"))
+    @all_events = Event.where('event_start>?',
+    DateTime.now.change(:offset => "+0000"))
 
     # @bookings = Bookroom.where(user_id: @user)
   end
