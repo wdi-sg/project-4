7d1df3de72f12f85352e6e23a6d4dad43f3c2eca
Azmeer Siddiq <reemza77@gmail.com>
2017-09-07 20:12:43 +0800

    edited first login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 before_action :authenticate_user!
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :isAdmin])
   end
+
+  def after_sign_in_path_for(resource)
+   if current_user.sign_in_count == 1
+     edit_user_registration_path
+   else
+     root_path
+   end
+  end
+
 end
