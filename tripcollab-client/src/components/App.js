76fa2bd21ec679b0356c981897fc79747c0c5f0d
Lim Jiechao <jiechao.lim@gmail.com>
2018-02-21 10:37:49 +0800

    responsive landing page
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- tripcollab-client/src/components/App.js
+++ tripcollab-client/src/components/App.js
@@ -1,54 +1,16 @@
 import React, { Component } from 'react';
-import {
-  Container,
-  Row,
-  Col,
-  Input,
-  Label,
-  Form,
-  FormGroup
-} from 'reactstrap';
-import '../styles/App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Locations from './Locations'
-import Itinerary from './Itinerary'
-import PlacesWithStandaloneSearchBox from './SearchBox';
-// import Alternative from './Alternative'
+// import Navmenu from '../components/Navmenu';
+import Home from '../components/Home';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Container>
-          <Row>
-            <Col>
-              <FormGroup className="md-4">
-                <Label for="exampleDate">Date</Label>
-                <Input type="date" name="date" id="exampleDate" placeholder="date placeholder" />
-              </FormGroup>
-            </Col>
-            <Col>
-              <FormGroup className="md-4">
-                <Label for="exampleDate">Date</Label>
-                <Input type="date" name="date" id="exampleDate" placeholder="date placeholder" />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="col-8">
-              <PlacesWithStandaloneSearchBox onAdd={this.addToList}/>
-            </Col>
-            <Col className="col-4">
-              <Locations/>
-            </Col>
-          </Row>
-          <Row className="mt-5">
-            <Col>
-              <Itinerary/>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
     );
   }
 }
--- tripcollab-client/src/components/Home.js
+++ tripcollab-client/src/components/Home.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
-
 import { Link } from 'react-router-dom';
-import { Container, Row, Col,  Button} from 'reactstrap';
-// import { Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  InputGroup,
+  Input,
+  InputGroupAddon
+} from 'reactstrap';
 import '../styles/App.css';
 
 
@@ -10,46 +16,54 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1 class="logo text-center">TripCollab</h1>
-        <h2 class="main-text">Welcome!</h2>
-        <h3 class="main-text">TripCollab can help to plan trip with your Friends</h3>
-        <Container id="back-panel">
-          <Row className="text-center">
-            <Col xs={12} sm={3}>
-              <img src="assets/planning.svg" className="image-icon"/>
-              <h4 class="icon-header">Easy Trip Planning</h4>
-              <p>Able to quickly create and arrange itinerary.</p>
-            </Col>
-
-            <Col xs={12} sm={3}>
-              <img src="assets/calendar.svg" className="image-icon"/>
-              <h4 class="icon-header">Personalised Itinerary</h4>
-              <p>Lorem ipsum dolor sit ame adipiscing elit.</p>
-            </Col>
-
-            <Col xs={12} sm={3}>
-              <img src="assets/add-contact.svg" className="image-icon"/>
-              <h4 class="icon-header">Seamless Collaboration</h4>
-              <p>Lorem ipsum dolor sit ame adipiscing elit.</p>
-            </Col>
-
-            <Col xs={12} sm={3}>
-              <img src="assets/offline.svg" className="image-icon"/>
-              <h4 class="icon-header">Offline Access</h4>
-              <p>Lorem ipsum dolor sit ame adipiscing elit..</p>
+        <Container>
+          <h1 className="logo text-center">TripCollab</h1>
+          <Row>
+            <Col>
+              <h3 className="main-text text-center">Plan a trip with your friends now!</h3>
             </Col>
           </Row>
         </Container>
 
-      <Row>
-        <Col className="">
-        <Link to="/Itinerary">
-          <Button className="text-center primary">Go</Button>
-        </Link>
-      </Col>
-      </Row>
-        <footer className='navbar-fixed-bottom'>
-          <p>Copyright © 2018 <a href='https://github.com/chongct/project-4'><strong>TripCollab</strong></a>. All Rights Reserved.</p>
+        <Container id="text-field-container" className="d-flex justify-content-center align-items-center">
+          <InputGroup id="destination-field">
+            <Input placeholder="Where are you going to?"/>
+            <InputGroupAddon addonType="append">
+              <Button id="go-button" color="primary">Go!</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Container>
+
+        <Container fluid id="back-panel">
+          <Row className="text-center">
+            <Col xs={6} sm={3}>
+              <img src="assets/planning.svg" alt="planner-icon" className="image-icon"/>
+              <h4 className="icon-header">Easy Trip Planning</h4>
+              <p className="panel-text">Able to quickly create and arrange itinerary.</p>
+            </Col>
+
+            <Col xs={6} sm={3}>
+              <img src="assets/calendar.svg" className="image-icon" alt="calendar icon"/>
+              <h4 className="icon-header">Personalised Itinerary</h4>
+              <p className="panel-text">Lorem ipsum dolor sit ame adipiscing elit.</p>
+            </Col>
+
+            <Col xs={6} sm={3}>
+              <img src="assets/add-contact.svg" alt="add contact icon" className="image-icon"/>
+              <h4 className="icon-header">Seamless Collaboration</h4>
+              <p className="panel-text">Lorem ipsum dolor sit ame adipiscing elit.</p>
+            </Col>
+
+            <Col xs={6} sm={3}>
+              <img src="assets/offline.svg" alt="no Wi-Fi icon" className="image-icon"/>
+              <h4 className="icon-header">Offline Access</h4>
+              <p className="panel-text">Lorem ipsum dolor sit ame adipiscing elit..</p>
+            </Col>
+          </Row>
+        </Container>
+
+        <footer className="text-center text-light">
+          <p>Copyright © 2018 <a id="github" href='https://github.com/chongct/project-4'><strong>TripCollab</strong></a>. All Rights Reserved.</p>
         </footer>
       </div>
     )
