ed155efa08d3d3abed758b1ab1c63e3ff7b5279f
Darren Koh <darren212@gmail.com>
2018-02-22 19:14:27 +0800

    Updated Backend Routes

--- tripcollab-server/routes/routes.js
+++ tripcollab-server/routes/routes.js
@@ -8,7 +8,7 @@ const homeController = require('../controllers/homeController')
 
 //==================== Trip Control ====================
 router.post('/trip/new', tripController.create) // create
-router.get('/trip/:id', tripController.read) // read
+router.get('/trip/read/:id', tripController.read) // read
 router.post('/trip/update/:id', tripController.update) // update
 // router.post('/trip/delete', tripController.delete)
 
